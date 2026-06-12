7815c88d823cd3f4b955b80fa2d0382bfd3337c2
sosiksaWteste <galnikser@gmail.com>
2026-06-12 22:48:52 +0300

    fix material auth

--- db_lab_backend/routes/material.js
+++ db_lab_backend/routes/material.js
@@ -12,13 +12,13 @@ const { isAdmin, isStudent } = require('../middlewares/auth.js');
 const upload = require('../middlewares/multerMaterial.js');
 const router = express.Router();
 
-router.post('/create', create);
+router.post('/create', isAdmin, create);
 router.get('/getAll', getAll);
-router.delete('/delete/:material_Id', deleter);
-router.put('/:material_Id', update);
+router.delete('/delete/:material_Id', isAdmin, deleter);
+router.put('/:material_Id', isAdmin, update);
 router.get('/getFromDb', getFromDb);
 
-router.post('/:material_Id/upload-file', upload.single('file'), uploadFile);
+router.post('/:material_Id/upload-file', isAdmin, upload.single('file'), uploadFile);
 router.get('/file/:filename', serveMaterialFile);
 
 module.exports = router;
